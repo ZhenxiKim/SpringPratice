5519583ec7e687fef01bc578fc975330ffe8980d
zhenxiKim <8055kjh@gmail.com>
2022-05-18 18:12:28 +0900

    sql문 추가

--- src/test/java/com/example/springpratice/repository/UserRepositoryTest.java
+++ src/test/java/com/example/springpratice/repository/UserRepositoryTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -17,9 +19,16 @@ class UserRepositoryTest {
     private UserRepository userRepository;
 
     @Test
+    @Transactional
     void crud() {
+        /*
+        List<User> users = userRepository.findAll(Sort.by(Direction.DESC, "name"));
+        users.forEach(System.out::println);
         userRepository.save(new User());
 
         System.out.println(">>>>" + userRepository.findAll());
+        */
+        User user = userRepository.getOne(1L);
+        System.out.println(user);
     }
 }
---
+++ src/test/java/resources/data.sql
@@ -0,0 +1,14 @@
+call next value for hibernate_sequence;
+insert into user(`id`, `name`, `email`,`created_at`,`updated_at`) values (1,'martin','martin@gmail.com',now(),now());
+
+call next value for hibernate_sequence;
+insert into user(`id`, `name`, `email`,`created_at`,`updated_at`) values (2,'jay','test2@gmail.com',now(),now());
+
+call next value for hibernate_sequence;
+insert into user(`id`, `name`, `email`,`created_at`,`updated_at`) values (3,'james','test3@gmail.com',now(),now());
+
+call next value for hibernate_sequence;
+insert into user(`id`, `name`, `email`,`created_at`,`updated_at`) values (4,'sopia','sopia@gmail.com',now(),now());
+
+call next value for hibernate_sequence;
+insert into user(`id`, `name`, `email`,`created_at`,`updated_at`) values (5,'martin','martinAnother@gmail.com',now(),now());
