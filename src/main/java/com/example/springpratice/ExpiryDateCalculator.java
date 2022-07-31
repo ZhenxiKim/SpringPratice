5f8d490950428e25dfe8214e09ed782c4188f596
zhenxiKim <8055kjh@gmail.com>
2022-07-31 22:26:21 +0900

    test : ExpiryDateCalculator

---
+++ src/main/java/com/example/springpratice/ExpiryDateCalculator.java
@@ -0,0 +1,13 @@
+package com.example.springpratice;
+
+import java.time.LocalDate;
+
+/**
+ * @author jhkim
+ * @since 2022-07-31
+ */
+public class ExpiryDateCalculator {
+    public LocalDate calculateExpiryDate(LocalDate billingDate, int payAmount) {
+        return LocalDate.of(2019,4,1);
+    }
+}
--- src/test/java/com/example/springpratice/repository/UserRepositoryTest.java
+++ src/test/java/com/example/springpratice/repository/UserRepositoryTest.java
@@ -5,9 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author jhkim
@@ -19,16 +16,12 @@ class UserRepositoryTest {
     private UserRepository userRepository;
 
     @Test
-    @Transactional
     void crud() {
-        /*
-        List<User> users = userRepository.findAll(Sort.by(Direction.DESC, "name"));
-        users.forEach(System.out::println);
-        userRepository.save(new User());
+        userRepository.save(new User("david", "david@fastcampus.com"));
 
-        System.out.println(">>>>" + userRepository.findAll());
-        */
-        User user = userRepository.getOne(1L);
-        System.out.println(user);
+        User user = userRepository.findById(1L).orElseThrow(RuntimeException::new);
+        user.setEmail("martin-update@fastcampus.com");
+
+        userRepository.save(user);
     }
 }
