86bbd7a8227cf833ca2a022df5ec090d8f5ef256
zhenxiKim <8055kjh@gmail.com>
2022-09-14 22:50:36 +0900

    회원 기능 테스트

---
+++ src/test/java/com/example/springpratice/service/MemberServiceTest.java
@@ -0,0 +1,50 @@
+package com.example.springpratice.service;
+
+import com.example.springpratice.domain.Member;
+import com.example.springpratice.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author jhkim
+ * @since 2022-09-14
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception {
+        Member member = new Member();
+        member.setName("kim");
+
+        Long savedId = memberService.join(member);
+
+        assertEquals(member, memberRepository.find(savedId));
+
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void 중복_회원_예외() throws Exception {
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        memberService.join(member1);
+        memberService.join(member2);
+        fail("예외가 발생해야 한다.");
+    }
+}
